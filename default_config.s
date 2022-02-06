28e9844351258354d5db28f27f4ce890356c0fc2
Timo Kokkonen <tjko@iki.fi>
2022-02-05 17:46:21 -0800

    Add comments and align data at 4-byte boundary.

--- default_config.s
+++ default_config.s
@@ -1,8 +1,23 @@
-.section .rodata
-.global fanpico_default_config_start
+# default_config.s
+# Copyright (C) 2021-2022 Timo Kokkonen <tjko@iki.fi>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This file is part of FanPico.
+#
+
+#
+# Stub for embedding default_config.json in the executable.
+#
+
+	.global fanpico_default_config_start
+	.global fanpico_default_config_end
+
+	.section .rodata
+	.p2align 2
 fanpico_default_config_start:
-.incbin "default_config.json"
-.byte 0x00
-.global fanpico_default_config_end
+	.incbin	"default_config.json"
+	.byte	 0x00
 fanpico_default_config_end:
 
+# eof
