7c14ab2efd04e07b10da790a3b3b70570bf42ef2
Timo Kokkonen <tjko@iki.fi>
2022-02-05 20:06:46 -0800

    Hack to make sure default_config.s gets recompiled if .json file has been updated.

--- build.sh
+++ build.sh
@@ -9,6 +9,18 @@ error() { echo "`basename $0`: $*"; }
 fatal() { echo "`basename $0`: $*"; exit 1; }
 
 
+# asm hack (CMake cant seem to handle .incbin in .s file)
+for s_file in *.s; do
+	d_file=$(echo $s_file | sed -e 's/\.s/.json/')
+	if [ -s "$d_file" ]; then
+		if [ "$d_file" -nt "$s_file" ]; then
+			echo "$d_file updated: touch $s_file"
+			touch "$s_file"	
+		fi
+	fi
+done
+
+
 [ -d "${BUILD_DIR}" ] || mkdir -p "${BUILD_DIR}" || fatal "failed to create: $BUILD_DIR"
 
 cd "${BUILD_DIR}" || fatal "cannot access: $BUILD_DIR"
