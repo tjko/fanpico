b65380f7b7e04e03aa3696ee3d6a26102cef0da3
John Zitterkopf <zittware@hotmail.com>
2024-08-08 23:15:31 -0500

    Add ADC Vref as sensor 4. Allows the Vref to be measured and recorded as a constant for more accurate temperature sensor measurments.

--- src/config.c
+++ src/config.c
@@ -494,6 +494,7 @@ void clear_config(struct fanpico_config *cfg)
 	cfg->local_echo = false;
 	cfg->spi_active = false;
 	cfg->serial_active = false;
+	cfg->adc_vref = ADC_REF_VOLTAGE; //Zitt
 	cfg->led_mode = 0;
 	strncopy(cfg->name, "fanpico1", sizeof(cfg->name));
 	strncopy(cfg->display_type, "default", sizeof(cfg->display_type));
@@ -564,6 +565,7 @@ cJSON *config_to_json(const struct fanpico_config *cfg)
 	cJSON_AddItemToObject(config, "led_mode", cJSON_CreateNumber(cfg->led_mode));
 	cJSON_AddItemToObject(config, "spi_active", cJSON_CreateNumber(cfg->spi_active));
 	cJSON_AddItemToObject(config, "serial_active", cJSON_CreateNumber(cfg->serial_active));
+	cJSON_AddItemToObject(config, "adc_vref", cJSON_CreateNumber(cfg->adc_vref)); //Zitt
 	if (strlen(cfg->display_type) > 0)
 		cJSON_AddItemToObject(config, "display_type", cJSON_CreateString(cfg->display_type));
 	if (strlen(cfg->display_theme) > 0)
@@ -846,6 +848,10 @@ int json_to_config(cJSON *config, struct fanpico_config *cfg)
 		cfg->spi_active = cJSON_GetNumberValue(ref);
 	if ((ref = cJSON_GetObjectItem(config, "serial_active")))
 		cfg->serial_active = cJSON_GetNumberValue(ref);
+	//Zitt: BEGIN
+	if ((ref = cJSON_GetObjectItem(config, "adc_vref")))	
+	    cfg->adc_vref = cJSON_GetNumberValue(ref);
+	//Zitt: END
 	if ((ref = cJSON_GetObjectItem(config, "display_type"))) {
 		if ((val = cJSON_GetStringValue(ref)))
 			strncopy(cfg->display_type, val, sizeof(cfg->display_type));
