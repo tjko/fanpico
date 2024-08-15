29ca72f1edfe1154e1fdb5bec32baa07e55fb5d7
Timo Kokkonen <tjko@iki.fi>
2024-08-15 00:10:57 -0700

    Update uptime string layout on 128x64 displays.

--- src/display_oled.c
+++ src/display_oled.c
@@ -354,12 +354,11 @@ void oled_display_status(const struct fanpico_state *state,
 	}
 	else if (FAN_COUNT <= 4) {
 		/* Uptime / Clock */
-		snprintf(buf, sizeof(buf), "%02lu+%02lu:%02lu:%02lu",
-			days % 100,
+		snprintf(buf, sizeof(buf), "%03lu+%02lu:%02lu",
+			days % 1000,
 			hours % 24,
-			mins % 60,
-			secs % 60);
-		oledWriteString(&oled, 0, 0, 7, buf, FONT_6x8, 0, 1);
+			mins % 60);
+		oledWriteString(&oled, 0, 6, 7, buf, FONT_6x8, 0, 1);
 		if (rtc_get_datetime(&t)) {
 			snprintf(buf, sizeof(buf), "%02d:%02d", t.hour, t.min);
 			oledWriteString(&oled, 0, 3, 5, buf, FONT_12x16, 0, 1);
